e9fe0b2c79f340e1257f2e317fdb8ed4820ae3e9
deepmud <73426734+deepmud@users.noreply.github.com>
2021-03-04 06:55:11 +0100

    initial commit. PLANTED BUG

---
+++ src/main/java/anikputa/daniel/chiim/timedemo/README.md
@@ -0,0 +1,2 @@
+##hshdhdhf
+tyutrtytytyeeyeftydrddr
--- src/main/java/anikputa/daniel/chiim/timedemo/TimeDemoApplication.java
+++ src/main/java/anikputa/daniel/chiim/timedemo/TimeDemoApplication.java
@@ -5,12 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TimeDemoApplication {
-	public TimeDemoApplication(){
+	public TimeDemoApplication() {
 		System.out.println("hello world");
 	}
-GZZGHGJHK
-	public static void main(String[] args) {
-		SpringApplication.run(TimeDemoApplication.class, args);
+	GZZGHGJHK
+	JJKLKKKKKKKKKK
+			MM, KLNKN,
+	,JKJKJ
+	KJLKKN
+	public static void main(String[] args) {KKKKKL
+			BMMVVGHCGCCHJJJJ
+			NNJJHVHJVV VBVBV
+					JHHHFGHYTITFDIFD
+
 	}
 
 }
